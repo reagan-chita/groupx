cc06499fa5a71673fb76c0763bd110f3f7424a9f
reagan <reaganchita.com>
2026-03-22 03:54:01 +0200

    changes

--- app.py
+++ app.py
@@ -785,13 +785,13 @@ def upload_students():
 
 
 # ---------------- RUN ----------------
-# if __name__ == "__main__":
-#     with app.app_context():
-#         db.create_all()
-
-#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 4000)), debug=True)
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 4000)), debug=True)
+# if __name__ == "__main__":
+#     with app.app_context():
+#         db.create_all()
+
+#     app.run(debug=True)
